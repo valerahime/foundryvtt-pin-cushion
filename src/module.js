8f5cf2ab6f79d71f95c8824889126c06d7d6c6e8
p4535992 <tentimarco0@gmail.com>
2024-02-11 18:45:57 +0100

    revert jal flag

--- src/module.js
+++ src/module.js
@@ -257,13 +257,23 @@ Hooks.on("renderNoteConfig", async (app, html, noteData) => {
       return options;
     }
     // <select name="flags.anchor.slug">${getOptions(noteData.document.page, noteData.document.flags.anchor?.slug)}</select>
-    let anchorData = getProperty(noteData.document.flags, `${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}`); // noteData.document.flags.anchor;
+    // let anchorData = getProperty(noteData.document.flags, `${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}`); // noteData.document.flags.anchor;
+    let anchorData = getProperty(noteData.document.flags, `anchor`); // noteData.document.flags.anchor;
     let pageData = noteData.document.page;
+    // let select = $(`
+    // <div class='form-group'>
+    // 	<label>${i18n(`${CONSTANTS.MODULE_ID}.PageSection`)}</label>
+    // 	<div class='form-fields'>
+    // 		<select name="flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}.slug">
+    // 			${getOptions(pageData, anchorData?.slug)}
+    // 		</select>
+    // 	</div>
+    // </div>`);
     let select = $(`
 		<div class='form-group'>
 			<label>${i18n(`${CONSTANTS.MODULE_ID}.PageSection`)}</label>
 			<div class='form-fields'>
-				<select name="flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}.slug">
+				<select name="flags.anchor.slug">
 					${getOptions(pageData, anchorData?.slug)}
 				</select>
 			</div>
@@ -281,13 +291,12 @@ Hooks.on("renderNoteConfig", async (app, html, noteData) => {
       // getOptions(newpage, data.document.flags.anchor?.slug))
       log("new options =" + getOptions(newpage, anchorData?.slug));
       // app.form.elements["flags.anchor.slug"].innerHTML = getOptions(newpage, data.document.flags.anchor?.slug);
-      app.form.elements[`flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}.slug`].innerHTML = getOptions(
-        newpage,
-        anchorData?.slug
-      );
+      // app.form.elements[`flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}.slug`].innerHTML = getOptions(
+      app.form.elements[`flags.anchor.slug`].innerHTML = getOptions(newpage, anchorData?.slug);
       // app.form.elements["flags.anchor.slug"].innerHTML
       log(
-        "new innerHtml" + app.form.elements[`flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}.slug`].innerHTML
+        // "new innerHtml" + app.form.elements[`flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}.slug`].innerHTML
+        "new innerHtml" + app.form.elements[`flags.anchor.slug`].innerHTML
       );
     }
     html.find("select[name='entryId']").change(_updateSectionList);
@@ -935,7 +944,8 @@ Hooks.on("dropCanvasData", (canvas, data) => {
 Hooks.on("activateNote", (note, options) => {
   const enableJournalAnchorLink = game.settings.get(CONSTANTS.MODULE_ID, "enableJournalAnchorLink");
   if (enableJournalAnchorLink && !game.modules.get("jal")?.active) {
-    let anchorData = getProperty(note, `document.flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}`);
+    // let anchorData = getProperty(note, `document.flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.JAL_ANCHOR}`);
+    let anchorData = getProperty(note, `document.flags.anchor.slug`);
     options.anchor = anchorData?.slug;
     //options.anchor = note.document.flags.anchor?.slug;
   }
